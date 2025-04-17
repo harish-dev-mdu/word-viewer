b4694cef0353093b76f4df9baa0750e73a6ed27e
Harish kumar A <usernameissharish@gmail.com>
2025-04-17 17:05:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DocxViewer from './components/DocxViewer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Word Document Viewer</h1>
+      <DocxViewer />
+    </div>
+  );
+}
+
+export default App;
